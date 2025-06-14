99b2fb4c3aaf5e690a283f0421b7895cd9fccad1
combrain <skrtn3410@gmail.com>
2025-06-14 17:59:22 +0900

    refactor: Logo 컴포넌트 변경

--- modules/home/ui/components/header/logo/index.tsx
+++ modules/home/ui/components/header/logo/index.tsx
@@ -1,12 +1,28 @@
-import Image from "next/image";
-import styles from "./logo.module.scss";
+import Link from "next/link";
 
 export const Logo = () => {
   return (
-    <h1 className={styles.logo}>
-      <a href="">
-        <Image src="/image/devlog.png" alt="Logo" width={50} height={50} />
-      </a>
+    <h1>
+      <Link href="/">
+        <svg
+          width="120"
+          height="40"
+          viewBox="0 0 120 40"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <text
+            x="10"
+            y="25"
+            fill="currentColor"
+            fontSize="16"
+            fontWeight="bold"
+            fontFamily="Arial, sans-serif"
+          >
+            Combrain-Log
+          </text>
+        </svg>
+      </Link>
     </h1>
   );
 };
