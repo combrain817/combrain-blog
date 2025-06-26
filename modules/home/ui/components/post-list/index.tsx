dd191f89d2b2bb8f9fb1c2aef04b23a841a793c3
combrain <skrtn3410@gmail.com>
2025-06-26 23:08:20 +0900

    refactor: 글 없을 경우 UI 추가

--- modules/home/ui/components/post-list/index.tsx
+++ modules/home/ui/components/post-list/index.tsx
@@ -4,7 +4,15 @@ import { getMdxArticles } from "@/lib/mdx";
 
 export const PostList = () => {
   const posts = getMdxArticles();
-  
+
+  if (posts.length === 0) {
+    return (
+      <main className={styles.postList}>
+        <p>게시글이 없습니다.</p>
+      </main>
+    );
+  }
+
   return (
     <main className={styles.postList}>
       <ul>
